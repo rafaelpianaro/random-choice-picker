5d02f3575c4577a82b520e35f575e18c7d445337
Rafael <rafaelpianaro@gmail.com>
2022-01-12 22:21:38 -0300

    *pickRandomTag *highlightTag *unHighlightTag

--- js/main.js
+++ js/main.js
@@ -5,16 +5,60 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        },10)
+        randomSelect()
+    }
 })
 
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
     // console.log('tags', tags) // get wherever typing without spacing
+
     tagsEl.innerHTML = ''
+
     tags.forEach(tag => {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
+}
+
+function randomSelect() {
+    const times = 30
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        // console.log('randomTag', randomTag)
+        if (randomTag !== undefined) {
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+	}
+    }, 100);
+    // stop
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
